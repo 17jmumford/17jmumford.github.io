8e99501926cb55293cc08f3ad8b38d9b66b20674
Jeremy Mumford <36290330+17jmumford@users.noreply.github.com>
2023-12-17 14:50:02 -0700

    Update chatbot UI and functionality

--- assets/js/chatbot.js
+++ assets/js/chatbot.js
@@ -1,5 +1,12 @@
 var messageHistory = [];
 
+document.getElementById('chat-input').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        event.preventDefault(); 
+        sendMessage(document.getElementById('chat-input').value);
+    }
+});
+
 document.getElementById('send-message').addEventListener('click', function() {
     var userInput = document.getElementById('chat-input').value;
     sendMessage(userInput);
@@ -9,6 +16,12 @@ function sendMessage(message) {
     messageHistory.push(message);
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
+        if (this.readyState == XMLHttpRequest.OPENED) {
+            showLoading(true); // Show loading icon when request is sent
+        }
+        if (this.readyState == 4) {
+            showLoading(false); // Hide loading icon on response or error
+        }
         if (this.readyState == 4 && this.status == 200) {
             displayMessage('Bot: ' + this.responseText);
         }
@@ -30,4 +43,8 @@ function displayMessage(message) {
     var chatMessages = document.getElementById('chat-messages');
     chatMessages.innerHTML += '<div>' + message + '</div>';
     document.getElementById('chat-input').value = ''; // Clear input field
+}
+
+function showLoading(show) {
+    document.getElementById('loading').style.display = show ? 'block' : 'none';
 }
