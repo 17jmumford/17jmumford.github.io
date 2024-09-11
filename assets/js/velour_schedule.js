98d512afe5d07fb87ba679eeeec93f14a4f06d1c
Jeremy Mumford <36290330+17jmumford@users.noreply.github.com>
2024-09-10 20:56:58 -0600

    fixed calendar links

--- assets/js/velour_schedule.js
+++ assets/js/velour_schedule.js
@@ -60,7 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
                             calendarButton.style.width = '48px';
                             calendarButton.style.height = '48px';
                             calendarButton.addEventListener('click', () => {
-                                addToGoogleCalendar(eventDate.textContent, eventTitle.textContent);
+                                addToGoogleCalendar(eventDate.textContent, eventTitle.textContent, eventData.artists);
                             });
 
                             // Append calendar button to event item
@@ -85,8 +85,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     // Function to add event to Google Calendar
-    function addToGoogleCalendar(date, title) {
-        const calendarUrl = `https://www.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(title)}&dates=${date}/${date}`;
+    function addToGoogleCalendar(date, title, artists) {
+        const [month, day, year] = date.split('/');
+        const startDate = new Date(Date.UTC(year, month - 1, day, 2, 0, 0)); // Set to 8:00 PM MDT (2:00 AM UTC)
+        const endDate = new Date(startDate.getTime() + 3 * 60 * 60 * 1000); // Assuming 3-hour event duration
+        const formattedStartDate = startDate.toISOString().replace(/-|:|\.\d\d\d/g, "");
+        const formattedEndDate = endDate.toISOString().replace(/-|:|\.\d\d\d/g, "");
+        const cal_title = title + " @ Velour: " + artists.join(", ");
+        const calendarUrl = `https://calendar.google.com/calendar/render?action=TEMPLATE&dates=${formattedStartDate}%2F${formattedEndDate}&location=135%20N%20University%20Ave%2C%20Provo%2C%20UT%2084601&text=${encodeURIComponent(cal_title)}`;
         window.open(calendarUrl, '_blank');
     }
 });
